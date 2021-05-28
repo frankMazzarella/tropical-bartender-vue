b259c67c2c4542a9aa2eaeebc66d769ee81943f9
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-05-28 00:48:29 -0400

    moving drink row into component

---
+++ src/components/AvailableDrinkRow.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="drink-item">
+    <span class="drink-name">{{ drink.name }}</span>
+    <div class="ingredients">
+      <span v-for="(ingredient, index) in drink.ingredients" :key="index">
+        {{ ingredient.name }}<span v-if="index + 1 !== drink.ingredients.length">,</span>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AvailableDrinkRow',
+  props: ['drink']
+}
+</script>
+
+<style scoped>
+.drink-item {
+  background: #191b1f;
+  border-radius: 2px;
+  text-align: left;
+  display: inline-block;
+  position: relative;
+  padding: 10px;
+  height: 50px;
+  width: 90%;
+  margin: 5px;
+  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
+  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
+}
+
+.drink-item:active {
+  box-shadow: 5px 5px 5px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+}
+
+.drink-name {
+  color: #67a5fb;
+  font-size: 1.3em;
+}
+
+.ingredients {
+  position: absolute;
+  bottom: 10px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  width: 95%;
+}
+</style>
--- src/views/DrinkList.vue
+++ src/views/DrinkList.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <h1>Tropical Drink List</h1>
-    <div class="drink-item" v-for="(drink, index) in drinkList" :key="index">
-      <span class="drink-name">{{ drink.name }}</span>
-      <div class="ingredients">
-        <span v-for="(ingredient, index) in drink.ingredients" :key="index">
-          {{ ingredient.name }}<span v-if="index + 1 !== drink.ingredients.length">,</span>
-        </span>
-      </div>
+    <div v-for="(drink, index) in drinkList" :key="index">
+      <available-drink-row :drink="drink" />
     </div>
     <div class="modal active">
       modal
@@ -18,9 +13,13 @@
 
 <script>
 import SocketIOClient from '../services/SocketIOClient';
+import AvailableDrinkRow from '../components/AvailableDrinkRow';
 
 export default {
   name: 'DrinkList',
+  components: {
+    AvailableDrinkRow
+  },
   data: () => ({
     socket: null,
     drinkList: []
@@ -49,38 +48,6 @@ export default {
 <style scoped>
 h1 {
   color: #e0723b;
-}
-
-.drink-item {
-  background: #191b1f;
-  border-radius: 2px;
-  text-align: left;
-  display: inline-block;
-  position: relative;
-  padding: 10px;
-  height: 50px;
-  width: 90%;
-  margin: 5px;
-  box-shadow: 0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24);
-  transition: all 0.3s cubic-bezier(.25,.8,.25,1);
-}
-
-.drink-item:active {
-  box-shadow: 5px 5px 5px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
-}
-
-.drink-name {
-  color: #67a5fb;
-  font-size: 1.3em;
-}
-
-.ingredients {
-  position: absolute;
-  bottom: 10px;
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  width: 95%;
 }
 
 .active {
