86cce2edcc09ba4d8af399f1bc8cf7becff6f596
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-06-14 22:38:19 -0400

    using NODE_ENV to choose server url

--- src/services/SocketIOClient.js
+++ src/services/SocketIOClient.js
@@ -1,12 +1,18 @@
 import io from 'socket.io-client';
 
 export default class SocketIOClient {
-  // TODO: this needs to be configured to detect when in dev
   static connectToDrinkList() {
-    return io('http://10.0.0.36:3000/drink-list');
+    return io(this.getURL('drink-list'));
   }
 
   static connectToOrderQueue() {
-    return io('http://10.0.0.36:3000/order-queue');
+    return io(this.getURL('order-queue'));
+  }
+
+  static getURL(path) {
+    if (process.env.NODE_ENV === 'development') {
+      return `localhost:3000/${path}`;
+    }
+    return `/${path}`;
   }
 }
