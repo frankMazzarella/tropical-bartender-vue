c8623911c0d7774ce7df91ef9c5db07ef0ade489
Frank Mazzarella <mazzarella.frank@gmail.com>
2021-06-15 22:48:38 -0400

    cleanup and adding header on drink order

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,3 @@
     <router-view/>
   </div>
 </template>
-
-// TODO: needs vue-insomnia
-// https://github.com/gorbypark/vue-insomnia
-
-<style>
-#app {
-  max-width: 1200px;
-  margin: auto;
-}
-</style>
--- src/views/DrinkList.vue
+++ src/views/DrinkList.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <h1>Tropical Drink List</h1>
-    <div v-for="(drink, index) in drinkList" :key="index">
+    <div class="header">
+      <div class="container">
+        <div class="header-left">Tropical Drink Night!</div>
+        <div class="header-right">{{ dateTime }}</div>
+      </div>
+    </div>
+    <div class="spacer" />
+    <div class="container" v-for="(drink, index) in drinkList" :key="index">
       <available-drink-row :drink="drink" />
     </div>
     <order-drink-modal />
   </div>
 </template>
+
+// TODO: needs vue-insomnia
+// https://github.com/gorbypark/vue-insomnia
 
 <script>
 import SocketIOClient from '../services/SocketIOClient';
@@ -21,12 +30,14 @@ export default {
   },
   data: () => ({
     socket: null,
-    drinkList: []
+    drinkList: [],
+    dateTime: ''
   }),
   created: function () {
     this.socket = SocketIOClient.connectToDrinkList();
     this.registerSocketLifecycleEvents();
     this.$on('order-drink', this.emitDrinkOrder);
+    this.startDateTime();
   },
   methods: {
     registerSocketLifecycleEvents() {
@@ -48,13 +59,84 @@ export default {
         console.log(`${drink.recipient} has ordered a ${drink.name}`);
         this.$emit('order-complete');
       });
+    },
+    startDateTime() {
+      const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday'
+      ];
+      const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'August',
+        'October',
+        'November',
+        'December'
+      ];
+      setInterval(() => {
+        const now = new Date();
+        const weekDay = days[now.getDay()];
+        const month = months[now.getMonth()];
+        const date = now.getDate();
+        const hours = now.getHours().toString().padStart(2, '0');
+        const minutes = now.getMinutes().toString().padStart(2, '0');
+        this.dateTime = `${weekDay}, ${month} ${date}, ${hours}:${minutes}`;
+      }, 1000);
     }
   }
 }
 </script>
 
 <style scoped>
-h1 {
+.container {
+  max-width: 1200px;
+  margin: auto;
+}
+
+.header {
+  background-color: #191b1f;
+  width: 100%;
+  position: fixed;
+  z-index: 50;
+  border-bottom: 1px solid #e0723b;
   color: #e0723b;
+  font-size: 1.5em;
+}
+
+.header-left {
+  float: left;
+  padding: 5px 15px;
+}
+
+.header-right {
+  float: right;
+  padding: 5px 15px;
+}
+
+.spacer {
+  height: 50px;
+}
+
+@media (max-width: 700px) {
+  .header-left {
+    float: none;
+    text-align: center;
+  }
+
+  .header-right {
+    display: none;
+  }
 }
 </style>
